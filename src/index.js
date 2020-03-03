7ce512495bcdd3d8d0434a865deda64355b112bd
Alexei Kravchuk <alexei.kravchuk21@gmail.com>
2020-03-03 15:28:34 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let str = "";
+    n = Math.abs(n);
+    while(n > 0) {
+        str += n % 10;
+        n = Math.floor(n / 10);
+    }
+  return Number(str);
 }
